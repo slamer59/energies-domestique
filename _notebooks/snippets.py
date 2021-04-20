b8d2b5eca84896e9f05bd19326a00268e6e5a5fd
Pedot T <thomas.pedot@gmail.com>
2021-04-20 10:57:52 +0200

    Add sankey

--- _notebooks/snippets.py
+++ _notebooks/snippets.py
@@ -15,5 +15,23 @@ def export_plot_fastpages(plot, filename):
             for line in file:
 
                     replacement_text = ""
-                    print(line.replace(text_to_search, replacement_text), end='')
+                    print(line.replace(text_to_search, replacement_text), end='')       
+                    
 
+def export_plot_fastpages_panel(plot, filename, options=None):
+    
+    if options:
+        plot.save(filename, **options)
+    else:
+        plot.save(filename)
+    
+    import fileinput
+    with open(filename, 'r') as original: data = original.read()
+    with open(filename, 'w') as modified: modified.write("{% raw %}\n" + data + "\n{% endraw %}")
+
+    for text_to_search in ["<!DOCTYPE html>", '<html lang="en">','</html>', '<head>', '</head>', '<body>','</body>']:        
+        with fileinput.FileInput(filename, inplace=True) as file:
+            for line in file:
+
+                    replacement_text = ""
+                    print(line.replace(text_to_search, replacement_text), end='')
